c8b194c302007c6e71dbb6a10a032970f52d1d0d
Maria-Jovince <150923260+jovince-felson@users.noreply.github.com>
2026-07-06 18:35:16 +0530

    Update salesReports.routes.js

--- routes/salesReports.routes.js
+++ routes/salesReports.routes.js
@@ -1,11 +1,8 @@
 import express from "express";
 import { protect, adminOrSales } from "../middlewares/auth.middleware.js";
 import indexControllers from "../controllers/index.controllers.js";
-import checkPlanFeature from "../middlewares/checkPlanFeature.js";
 
 const router = express.Router();
-
-router.use(checkPlanFeature("reports"));
 
 // Fetch sales performance metrics
 router.get("/performance", protect, adminOrSales, indexControllers.salesReportsController.getSalesPerformance);
